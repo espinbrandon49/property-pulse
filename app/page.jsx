055c812df0ba804ce27615f6f8419ad70d88f58c
Brandon Espinosa <espinbrandon49@gmail.com>
2024-05-27 16:43:11 -0400

    connect database to application in config folder

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,12 @@ import Hero from "@/components/Hero"
 import HomeProperties from "@/components/HomeProperties"
 import InfoBoxes from "@/components/InfoBoxes"
 
-
 const HomePage = () => {
-
   return (
     <>
       <Hero />
       <InfoBoxes />
-      <HomeProperties/>
+      <HomeProperties />
     </>
   )
 }
